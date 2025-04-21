116b5a7e4d1216d556b29d23f6c82bd3de1c93d7
sharath-yanamandra <30403425+sharath-yanamandra@users.noreply.github.com>
2025-04-21 13:36:07 +0530

    second commit

---
+++ updated_config.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    
+    DETECTION_MODEL_PATH = 'train/weights/best.pt'
+    # Processing Configuration
+    MAX_RETRIES = 3
+    PROCESSING_TIMEOUT = 3600 # 1 hour in seconds
+    FRAMES_OUTPUT_DIR = 'frames'
+
+    TRACKING_THRESHOLD = 6
+    MAX_AGE = 50
+    MIN_MA = 1
+MIN_TRACK_LENGTH = 5  # Minimum frames to consider a track valid
